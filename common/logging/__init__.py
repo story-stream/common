7efb2a1ffb04d3e3dc1e5bea2b4da69564a9335e
Gary Prescott <gary.prescott@storystream.it>
2015-05-29 14:55:23 +0100

    Initial commit.

---
+++ common/logging/__init__.py
@@ -0,0 +1,57 @@
+import logging as py_logging
+from common.logging import handlers
+
+__all__ = [
+    'get_logger',
+    'configure'
+]
+
+
+class LEVEL:
+    CRITICAL = 50
+    FATAL = CRITICAL
+    ERROR = 40
+    WARNING = 30
+    WARN = WARNING
+    INFO = 20
+    DEBUG = 10
+    NOTSET = 0
+
+
+def get_logger(name=None):
+    """
+    Return a logger with the specified name, creating it if necessary.
+
+    If no name is specified, return the root logger.
+    :param name: The name of the logger instance
+    :return: A log instance
+    """
+    return py_logging.getLogger(name)
+
+
+def getLogger(name=None):
+    """
+    Return a logger with the specified name, creating it if necessary.
+
+    If no name is specified, return the root logger.
+    :param name: The name of the logger instance
+    :return: A log instance
+    """
+    return get_logger(name)
+
+
+def configure(logger, config, level=LEVEL.INFO):
+    """
+    Configures a logger instance from a configuration section.
+
+    :param logger: The logger instance to configure
+    :param config: A configuration dictionary instance
+    :param level: The minimum logging level to listen for
+    :return: The configured logger
+    """
+    if not logger.handlers:
+        handler = handlers.create(config)
+        logger.addHandler(handler)
+        logger.setLevel(level)
+
+    return logger
---
+++ common/logging/handlers/mongo.py
@@ -0,0 +1,30 @@
+import logging as py_logging
+import pymongo
+
+
+class MongoHandler(py_logging.Handler):
+
+    def __init__(self, connection, level=py_logging.NOTSET):
+        client = pymongo.MongoClient(connection)
+        self.__connection = connection
+        self.db = client.get_default_database()
+
+        super(MongoHandler, self).__init__(level)
+
+    def connection(self):
+        return self.__connection
+
+    def emit(self, record):
+        log_model = {
+            'name': record.name,
+            'module': record.module,
+            'func_name': record.funcName,
+            'line_no': record.lineno,
+            'thread': record.thread,
+            'thread_name': record.threadName,
+            'process': record.process,
+            'message': record.msg,
+            'args': str(record.args)
+        }
+
+        self.db[record.levelname.lower()].insert(log_model)
