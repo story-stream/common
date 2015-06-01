e03ceb7045d00621d55ac87cba92d24ed5e51223
Gary Prescott <gary.prescott@storystream.it>
2015-06-01 09:44:21 +0100

    Updated version number in setup file to allow for pip update.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ packages = find_packages(exclude=['ex_setup'])
 
 setup(
     name='common',
-    version='0.0.1',
+    version='1.0.0.1',
     author='StoryStream',
     author_email='hello@storystream.it',
     description='A set of common libraries and utilities that are used across the StoryStream infrastructure.',
